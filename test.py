00db3de64c111ed72a55c63e0c53a426d343fd52
Dagim Fikru <dagimfikru@gmail.com>
2023-08-22 23:40:57 +0300

    testing the board

--- test.py
+++ test.py
@@ -5,4 +5,13 @@ def constBoard(board):
     print(board[3], "|", board[4], "|", board[5])
     print("---------")
     print(board[6], "|", board[7], "|", board[8])
+for i in range(9):
+    if i>0 and i%3==0:
+        print("\n")
+    if board[i]==0:
+        print("_", end=" ")
+    elif board[i]==-1:
+        print("X", end=" ")
+    else:
+        print("O", end=" ")
 constBoard(board)
